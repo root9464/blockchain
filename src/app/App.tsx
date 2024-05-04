32dcc147434cb7c5bc71dc3b161724e5b1955b2d
iO <senpai9464@yandex.ru>
2024-05-04 22:22:05 +0500

    add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,44 @@
-import {Table} from '../components/Table/table';
+import {PlatformData, Table} from '../components/Table/table';
 import './global.sass';
 function App() {
+	const json: PlatformData[] = [
+		{
+			name: 'pancakeswapv2',
+			money: {
+				first: 'bnb',
+				second: 'bsc-usdt',
+			},
+			amount: {
+				first: '0.04878654234',
+				second: '0.04878654234',
+			},
+		},
+		{
+			name: 'pancakeswapv2',
+			money: {
+				first: 'bnb',
+				second: 'bsc-usdt',
+			},
+			amount: {
+				first: '0.04878654234',
+				second: '0.04878654234',
+			},
+		},
+		{
+			name: 'pancakeswapv2',
+			money: {
+				first: 'bnb',
+				second: 'bsc-usdt',
+			},
+			amount: {
+				first: '0.04878654234',
+				second: '0.04878654234',
+			},
+		},
+	];
 	return (
 		<>
-			<Table />
+			<Table {...{json}} />
 		</>
 	);
 }
--- src/components/Table/table.tsx
+++ src/components/Table/table.tsx
@@ -1,6 +1,6 @@
 
 import style from './table.module.sass';
-type PlatformData = {
+export type PlatformData = {
   name: string;
   money: {
     first: string;
@@ -12,83 +12,45 @@ type PlatformData = {
   };
 };
 
-export const Table = () => {
-	const json: PlatformData[] = [
-		{
-			name: 'pancakeswapv2',
-			money: {
-				first: 'bnb',
-				second: 'bsc-usdt',
-			},
-			amount: {
-				first: '0.04878654234',
-				second: '0.04878654234',
-			},
-		},
-		{
-			name: 'pancakeswapv2',
-			money: {
-				first: 'bnb',
-				second: 'bsc-usdt',
-			},
-			amount: {
-				first: '0.04878654234',
-				second: '0.04878654234',
-			},
-		},
-		{
-			name: 'pancakeswapv2',
-			money: {
-				first: 'bnb',
-				second: 'bsc-usdt',
-			},
-			amount: {
-				first: '0.04878654234',
-				second: '0.04878654234',
-			},
-		},
-	];
+export const Table = ({json}: {json: PlatformData[]}) => (
+	<div className={style.table}>
+		<table>
+			<thead>
+				<tr>
+					<th><h1>Платформа</h1></th>
+					<th><h1>Основная монета</h1></th>
+					<th><h1>Колличество</h1></th>
+				</tr>
+			</thead>
 
-	return (
-		<div className={style.table}>
-			<table>
-				<thead>
-					<tr>
-						<th><h1>Платформа</h1></th>
-						<th><h1>Основная монета</h1></th>
-						<th><h1>Колличество</h1></th>
-					</tr>
-				</thead>
+			<tbody>
+				{
+					json.map((item: PlatformData) => (
+						<div className={style.table}>
 
-				<tbody>
-					{
-						json.map((item: PlatformData) => (
-							<div className={style.table}>
+							<tr>
+								<td>
+									<p>{item.name}</p>
+								</td>
+							</tr>
+
+							<tr className={style.containertr}>
+								<tr>
+									<td><p>{item.money.first}</p></td>
+									<td><p>{item.money.second}</p></td>
+								</tr>
 
 								<tr>
-									<td>
-										<p>{item.name}</p>
-									</td>
+									<td><p>{item.amount.first}</p></td>
+									<td><p>{item.amount.second}</p></td>
 								</tr>
+							</tr>
 
-								<tr className={style.containertr}>
-									<tr>
-										<td><p>{item.money.first}</p></td>
-										<td><p>{item.money.second}</p></td>
-									</tr>
+						</div>
+					))
+				}
+			</tbody>
 
-									<tr>
-										<td><p>{item.amount.first}</p></td>
-										<td><p>{item.amount.second}</p></td>
-									</tr>
-								</tr>
-
-							</div>
-						))
-					}
-				</tbody>
-
-			</table>
-		</div>
-	);
-};
+		</table>
+	</div>
+);
